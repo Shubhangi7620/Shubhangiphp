54c5d09384f3b90481c9f6db7b491a6f47babf2e
Shubhangi <shubhangibhedekar@gmail.com>
2023-04-18 14:20:53 +0530

    Only one link active now
    Only one home button activation is on.

--- FlexStart/index.php
+++ FlexStart/index.php
@@ -5,7 +5,23 @@
 include 'head.php';
 ?>
 
+
 <body>
+<script>
+    const links = document.querySelectorAll('.nav-link');
+    
+    if (links.length) {
+      links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+          links.forEach((link) => {
+              link.classList.remove('active');
+          });
+          e.preventDefault();
+          link.classList.add('active');
+        });
+      });
+    }
+  </script>
 
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top">
